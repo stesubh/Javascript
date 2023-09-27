3ebc7778e06e134bd5d929fe4ac72e10ff1fe513
Subhadip Das <40082792+stesubh@users.noreply.github.com>
2023-09-27 20:32:41 +0000

    Datatypes Done!

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3)
+// alert("Hii Subhadip")   We are using node.js not browser
+
+/* console.log(3 +
+    3);
+console.log("Subhadip"); console.log("Das") */ //Code readability should be high
+
+
+let name = "Subhadip"
+let age = 24
+let isLoggedIn = false
+
+// Primitive Datatypes =>
+// Number
+// BigInt
+// String => "" / ''
+// Boolean => True / false
+// null => Standalone value
+// undefined
+// symbol => Defines uniqueness
+
+// Non-primitive Datatypes =>
+// object
+
+
+console.log(typeof undefined)  //=> undefined
+console.log(typeof null);  //=> Object
+

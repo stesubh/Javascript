f36f777543b5a3b559c17467d5e702500d2318e8
Subhadip Das <40082792+stesubh@users.noreply.github.com>
2023-09-28 21:53:30 +0000

    Conversion of Datatypes Done!

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,41 @@
+// let score = "33subh"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = false
+let score = "Subh"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+
+
+// "33" => 33
+// "33abc" => NaN(Not a Number)
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+
+let isLoggedIn = undefined
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false
+// "subhadip" => true
+// null => false
+// undefined => false
+
+let someNumber = 36
+let stringSomeNumber = String(someNumber)
+
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
